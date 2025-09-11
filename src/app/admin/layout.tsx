f570d084b38a8415f2ab246e1a508cdb525e11b3
Chandramouli Joshi <joshichandramouli2005@gmail.com>
2025-09-11 18:46:26 +0000

    if i go to admin panel all the buttons next to it are becomingvertica  (

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -7,7 +7,9 @@ export default function AdminLayout({
 }) {
   return (
     <div className="flex min-h-screen w-full flex-col">
-      <Header />
+       <header className="flex h-16 items-center gap-2 border-b bg-card px-4 lg:px-6">
+        <Header />
+      </header>
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6 bg-background">
         {children}
       </main>
