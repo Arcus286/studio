d6490c36766fbd55b1013ea272f7157a57796a9d
Akshay V <akshayvenkat02@gmail.com>
2025-09-17 20:30:53 +0000

    Try fixing this error: `Runtime Error: Error: A <Select.Item /> must hav

--- src/components/tasks/new-task-dialog.tsx
+++ src/components/tasks/new-task-dialog.tsx
@@ -215,7 +215,6 @@ export function NewTaskDialog({ children }: { children: React.ReactNode }) {
                           </SelectTrigger>
                         </FormControl>
                         <SelectContent>
-                          <SelectItem value="">None</SelectItem>
                           {stories.map(story => (
                             <SelectItem key={story.id} value={story.id}>{story.title}</SelectItem>
                           ))}
