63f5ed6e6cab8e22cc1423e7196db9f4f3269662
Chandramouli Joshi <joshichandramouli2005@gmail.com>
2025-10-17 07:09:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "agilebridgenewalph-91891-b1884",
+  "appId": "1:37690554871:web:be68d79d273049d51596f2",
+  "apiKey": "AIzaSyDAVodoNCYFd-pqu3gfNq0tuX3-m7vzP0Y",
+  "authDomain": "agilebridgenewalph-91891-b1884.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "37690554871"
+};
